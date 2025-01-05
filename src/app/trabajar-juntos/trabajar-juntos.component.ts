53ef6647acd3eb2a00b47f1759e16f6414be9521
Sebastian Zaruha <sebastianzaruha@gmail.com>
2025-01-05 14:15:19 +0100

    Inicio del proyecto

---
+++ src/app/trabajar-juntos/trabajar-juntos.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-trabajar-juntos',
+  imports: [],
+  templateUrl: './trabajar-juntos.component.html',
+  styleUrl: './trabajar-juntos.component.css'
+})
+export class TrabajarJuntosComponent {
+
+}
